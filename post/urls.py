4b0d7ce3c2dac1267e347131b5240f7ef47c0c46
Amit Parkar <83871869+amitp0@users.noreply.github.com>
2022-10-11 10:13:18 +0530

    Modified Post app

--- post/urls.py
+++ post/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path
-from django.conf import settings
-from django.conf.urls.static import static
 from .views import (PostDetail,PostList)
 urlpatterns = [
-    path('', PostList.as_view()),
-    path('api/post', PostList.as_view()),
-    path('api/post/<int:pk>', PostDetail.as_view()),
+    path('', PostList.as_view(),name='post_list'),
+    path('posts/<slug:slug>',PostDetail.as_view(),name='post_detail'),
+    path('v1/api/post', PostList.as_view()),
+    path('v1/api/post/<int:pk>', PostDetail.as_view()),
     
 ]
