fc87324764f31f066097d2458a9dbd0a8952a09b
Amit Parkar <83871869+amitp0@users.noreply.github.com>
2022-10-12 10:47:05 +0530

    Formatted the code

--- account/models.py
+++ account/models.py
@@ -4,3 +4,7 @@ from django.contrib.auth.models import AbstractUser
 class User(AbstractUser):
     about=models.TextField()
     profile_photo=models.ImageField(upload_to='static/images/users/')
+
+    def __str__(self):
+        return str(self.username)
+        
--- account/views.py
+++ account/views.py
@@ -1,57 +1,61 @@
-from rest_framework import status
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from .serializers import AccountSerializer
 from django.http import Http404
-from .models import User
+from rest_framework import status
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.renderers import TemplateHTMLRenderer
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from .models import User
+from .serializers import AccountSerializer
+
 # Create your views here.
 
 class AccountList(APIView):
-    def get(self,request):
-        Posts = User.objects.all()
-        serializer = AccountSerializer(Posts, many=True)
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+    def get(self, request):
+        posts = User.objects.all()
+        serializer = AccountSerializer(posts, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
-  
+
     def post(self, request):
         serializer = AccountSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data,status=status.HTTP_201_CREATED)
-        
-        
+
 class AccountDetail(APIView):
     renderer_classes = [TemplateHTMLRenderer]
     template_name = 'account/profile_detail.html'
-    def get_object(self, pk):
+    def get_object(self, primary_key):
         try:
-            return User.objects.get(pk=pk)
+            return User.objects.get(pk=primary_key)
         except User.DoesNotExist:
             raise Http404
 
-    def get(self,request, pk):
+    def get(self, request, pk):
         user = self.get_object(pk)
         serializer = AccountSerializer(user)
-        return Response({'serializer': serializer, 'profile': user})
-  
-    def put(self, request, pk):
-        user = self.get_object(pk)
+        return Response(serializer.data)
+
+    def put(self, request, primary_key):
+        user = self.get_object(primary_key)
         serializer = AccountSerializer(user, data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-  
-    def patch(self, request, pk):
-        user = self.get_object(pk)
+
+    def patch(self, request, primary_key):
+        user = self.get_object(primary_key)
         serializer = AccountSerializer(user,data=request.data,partial=True)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-          
-  
-    def delete(self,request, pk):
+
+    def delete(self, request, pk):
         user = self.get_object(pk)
         user.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
