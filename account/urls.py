8b03dcb723a432ea862f03da7b8948b3e834d5eb
Amit Parkar <83871869+amitp0@users.noreply.github.com>
2022-10-17 10:39:42 +0530

    Reformatted the URLs

--- account/urls.py
+++ account/urls.py
@@ -3,7 +3,6 @@ from django.urls import path, include
 from .views import (AccountDetail, AccountList)
 
 urlpatterns = [
-    path('api/v1/user/<int:pk>', AccountDetail.as_view(), name='profile_detail'),
-    path('api/v1/user', AccountList.as_view(), name='account_list'),
-    path('accounts/', include('django.contrib.auth.urls')),
+    path('', AccountList.as_view(), name='account_list'),
+    path('<int:pk>', AccountDetail.as_view(), name='account_detail'),
 ]
--- post/urls.py
+++ post/urls.py
@@ -3,8 +3,7 @@ from django.urls import path
 from .views import PostDetail, PostList
 
 urlpatterns = [
-    path('api/v1/post', PostList.as_view()),
-    path('api/v1/post/<int:pk>', PostDetail.as_view()),
-    path('', PostList.as_view(), name='post_list'),
-    path('posts/<slug:slug>', PostDetail.as_view(), name='post_detail'),
+    path('', PostList.as_view()),
+    path('<int:pk>', PostDetail.as_view()),
+    path('<slug:slug>', PostDetail.as_view()),
 ]
